2b0be34e1856d7906372e3b3886f2ce476d85ff7
Arthur <arthurtt08@gmail.com>
2023-03-28 20:03:37 -0300

    adicionando style ao cabecalhoe  e barra de pesquisa

---
+++ src/componentes/Cabecalho/index.jsx
@@ -0,0 +1,37 @@
+import { FaSearch } from "react-icons/fa"
+import cabecalho from "./Cabecalho.module.scss"
+import logo from "./logo.png"
+
+export default function Cabecalho() {
+  return (
+    <header>
+      <nav className="navbar">
+        <div
+          className={`container mt-3 d-flex align-items-center   ${cabecalho.ui}`}
+        >
+          <a className="navbar-brand d-flex align-items-center mb-5 me-0">
+            <img
+              className={`img-fluid ${cabecalho.imagem_logo}`}
+              src={logo}
+            ></img>
+            <h1 className={cabecalho.title_logo}>VidaRobótica</h1>
+          </a>
+          <form
+            className={`d-flex justify-content-center align-items-center  ${cabecalho.form_pesquisar}`}
+            role="search"
+          >
+            <span className={cabecalho.icone_pesquisar}>
+              <FaSearch size={25} />
+            </span>
+            <input
+              className={cabecalho.botao_pesquisa}
+              type="search"
+              placeholder="O que você procura"
+              aria-label="Search"
+            />
+          </form>
+        </div>
+      </nav>
+    </header>
+  )
+}
