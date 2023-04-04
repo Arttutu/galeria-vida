fc9f7709dd32f1294a7e7690056793b260129b3f
Arthur <arthurtt08@gmail.com>
2023-04-04 20:50:16 -0300

    teste

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,20 +1,28 @@
 import Cartao from "../Cartao"
 import Tags from "../Tags"
+import Populares from "../populares"
 import styles from "./Galeria.module.scss"
 export default function Galeria() {
   return (
-    <section className={styles.galeria}>
-      <div className="row">
-        <h2 className={`text-center mt-5 ${styles.galeria__titulo}`}>
-          Navegue pela galeria
-        </h2>
-      </div>
-      <div className="row">
-        <Tags></Tags>
-      </div>
-      <div className="row">
-        <Cartao />
-      </div>
-    </section>
+    <div className="container-fluid ">
+      <section className={`${styles.galeria}`}>
+        <div className="row">
+          <h2 className={`text-center mt-5 ${styles.galeria__titulo}`}>
+            Navegue pela galeria
+          </h2>
+        </div>
+        <div className="row">
+          <Tags></Tags>
+        </div>
+        <div className="row mt-5">
+          <div className="col-9">
+            <Cartao />
+          </div>
+          <div className="col-3">
+            <Populares />
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
